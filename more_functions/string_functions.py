5ce6b859b3b494a65b83a8d12297050825e2a751
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-26 20:22:08 -0600

    Test passes

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,3 +1,8 @@
 
 def multiply_strings(message, n):
-    pass
+    """Takes a message and echoes it back a given number of times.
+        :param message, a string to be echoed.
+        :param n, the number of times message will be echoed.
+        :returns the message multiplied as a string
+        """
+    return message * n
