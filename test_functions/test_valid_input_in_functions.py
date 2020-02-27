0b4b58d56348c19ee44076739d59339a011b6983
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-26 20:47:07 -0600

    First test passes

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -4,9 +4,9 @@ from more_functions import validate_input_in_functions
 
 class ValidateInputTest(unittest.TestCase):
     def test_score_input_test_name(self):
-        results = validate_input_in_functions.score_input()
+        results = validate_input_in_functions.score_input("Zachary", 100)
         expected = "Zachary: 100"
-        self.assertEqual(results, expected)
+        self.assertTrue(results == expected)
 
 
 if __name__ == '__main__':
