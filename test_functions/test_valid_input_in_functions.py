d6753828c6014743a523303d9d933f94633fdb77
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-26 20:49:44 -0600

    Second test passes

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -8,6 +8,11 @@ class ValidateInputTest(unittest.TestCase):
         expected = "Zachary: 100"
         self.assertTrue(results == expected)
 
+    def test_score_input_test_score_valid(self):
+        results = validate_input_in_functions.score_input("Zachary", 40)
+        expected = "Zachary: 40"
+        self.assertTrue(results == expected)
+
 
 if __name__ == '__main__':
     unittest.main()
